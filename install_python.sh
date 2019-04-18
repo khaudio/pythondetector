3f2b6867918056ebf8ada233b019505284b897c2
Kyle Hughes <kylehughes1@gmail.com>
2019-04-17 21:16:29 -0500

    updated shell script to use local temp folder

--- install_python.sh
+++ install_python.sh
@@ -5,6 +5,7 @@ DETECTORPATH=$PWD
 
 if [[ $($DETECTORPATH/bin/pythondetector $REQUIRED) != "minimum version installed" ]]; then
     echo "Installing python"
+    mkdir -p $DETECTORPATH/temp && cd $DETECTORPATH/temp
     sudo apt install -y git zlib1g-dev libffi-dev libssl-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev
     git clone git@github.com:python/cpython.git
     cd cpython

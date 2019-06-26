d6da42e01fb311288322e890f696599b86ee73a7
Kyle Hughes <kylehughes1@gmail.com>
2019-06-26 10:47:13 -0500

    updated install script using python2 compatible script within bash... maybe ok

--- install_python.sh
+++ install_python.sh
@@ -2,19 +2,30 @@
 
 REQUIRED="3.7.0"
 DETECTORPATH=$PWD
+VERSION=$(python -c \
+"print('$REQUIRED'.rpartition('.')[0] \
+if '$REQUIRED'.count('.') >= 2 \
+else '$REQUIRED')")
 
-if [[ ! -d $DETECTORPATH/bin ]]; then
-    cd $DETECTORPATH
-    make
-fi
+cd $DETECTORPATH
+make clean
+make
 
-if [[ $($DETECTORPATH/bin/pythondetector $REQUIRED) != "minimum version installed" ]]; then
+if [[ $($DETECTORPATH/bin/pythondetector $REQUIRED) \
+!= "minimum version installed" ]]; then
     echo "Installing python"
     mkdir -p $DETECTORPATH/temp && cd $DETECTORPATH/temp
-    sudo apt install -y git zlib1g-dev libffi-dev libssl-dev libbz2-dev libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev tk-dev uuid-dev libreadline-dev
-    git clone git@github.com:python/cpython.git
+    sudo apt update && sudo apt install -y \
+    git zlib1g-dev libffi-dev libssl-dev libbz2-dev \
+    libncursesw5-dev libgdbm-dev liblzma-dev libsqlite3-dev \
+    tk-dev uuid-dev libreadline-dev
+    git clone https://github.com/python/cpython.git
     cd cpython
+    git checkout $VERSION
+    make clean
     ./configure --enable-optimizations
     make
     sudo make install
+else
+    echo "Minimum python version installed"
 fi
